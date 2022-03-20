55ca03dfffcb88abe13d3ddeb14bae155769a047
ss-salah <salaheddinesalam@gmail.com>
2022-03-20 12:58:20 +0100

    SSM - BPro_1_11: Implement swagger only if profile is TEST

---
+++ src/main/java/com/soft/business/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.soft.business.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.soft.business.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().build());
+    }
+}
